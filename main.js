546bf9808308b5daa40eed99cb4e89aeac1ff035
mmuller2 <68875633+mmuller2@users.noreply.github.com>
2021-12-22 16:47:41 +0000

    stage 1 complete

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 /* 
 👉 Write your kata here!
+1- Given a string, write a function that reverses the string
+1 -Eg:
+hello = olleh
+brum = murb
+
+2- Given the reversed strings, create an array that hosts the newly found strings
+2- Eg:
+[olleh, murb]
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function reverseString(word) {
+  const reversing = word.split('').reverse().join('');
+  console.log(reversing);
+  return reversing;
+}
+//console.log(reverseString('best team ever'));
--- main.test.js
+++ main.test.js
@@ -1,1 +1,22 @@
 //👉 Write your tests below here:
+
+// import all the things
+
+import { reverseString } from './main.js';
+
+import jest from 'jest';
+
+// write a test in order to check the following ;
+//1- Given a string, write a function that reverses the string
+
+describe('reverseString', () => {
+  test('given a string, jest will test that it has been reversed by the provided function', () => {
+    //arrange
+    const word = 'olleh';
+    const expected = 'hello';
+    //act
+    const actual = reverseString(word);
+    //assert
+    expect(actual).toBe(expected);
+  });
+});
